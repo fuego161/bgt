d7a81c63a2773d183cfcb30805730af9366527da
fuego161 <46138241+fuego161@users.noreply.github.com>
2025-06-09 06:49:34 +0100

    Get the carousel items combined size, set default gap size

--- src/components/ui/carousel/index.tsx
+++ src/components/ui/carousel/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactElement, useCallback, useState } from "react";
+import { ReactElement, useCallback, useEffect, useState } from "react";
 
 import { CarouselItems } from "@/components/ui/carousel/items";
 
@@ -8,12 +8,31 @@ import type { CarouselPropsVariant } from "@/types/ui/carousel";
 
 interface CarouselPropsBase {
 	ariaLabel: string;
+	gapSize?: number;
 }
 
 type CarouselProps = CarouselPropsBase & CarouselPropsVariant;
 
 export const Carousel = (props: CarouselProps) => {
+	const gapSize = props.gapSize ?? 12;
+
 	const [itemSizes, setItemSizes] = useState<number[]>([]);
+	const [carouselLength, setCarouselLength] = useState<number>();
+
+	useEffect(() => {
+		// Get the amount of items stored
+		const itemCount = itemSizes.length;
+
+		// If there's no items yet, leave
+		if (!itemCount) return;
+
+		// Calculate the combined size of all the items
+		const itemsCombinedSize = itemSizes.reduce((a, b) => a + b, 0);
+		// Calculate the amount of gap between all the items, removing 1 for the final flush item
+		const combinedGapSize = gapSize * (itemCount - 1);
+
+		setCarouselLength(itemsCombinedSize + combinedGapSize);
+	}, [itemSizes, gapSize]);
 
 	const collectSize = useCallback(
 		(index: number) => (size: number) => {
@@ -27,6 +46,7 @@ export const Carousel = (props: CarouselProps) => {
 	);
 
 	console.log(props.type, itemSizes);
+	console.log(carouselLength);
 
 	const carouselItems: ReactElement =
 		props.type === "loader" ? (
@@ -37,10 +57,35 @@ export const Carousel = (props: CarouselProps) => {
 
 	return (
 		<nav {...(props.ariaLabel && { "aria-label": props.ariaLabel })}>
-			<div className="overflow-x-hidden">
-				<ul className="flex">
-					<CarouselItems {...props} />
-				</ul>
+			<div className="relative overflow-x-hidden">
+				<button
+					className="absolute left-0 top-0 bottom-0 bg-pink-300 px-2 z-20"
+
+					// className={buttonVariants({
+					// 	intent: "primary",
+					// })}
+					// disabled
+					// aria-disabled="true"
+					// aria-hidden="true"
+					// tabIndex={-1}
+				>
+					<span>&#x2190;</span>
+				</button>
+
+				<ul className="flex py-1">{carouselItems}</ul>
+
+				<button
+					className="absolute right-0 top-0 bottom-0 bg-pink-300 px-2 z-20"
+					// className={buttonVariants({
+					// 	intent: "primary",
+					// })}
+					// disabled
+					// aria-disabled="true"
+					// aria-hidden="true"
+					// tabIndex={-1}
+				>
+					<span>&#x2192;</span>
+				</button>
 			</div>
 		</nav>
 	);
