4c90923e5e06f3aac7cae1c08d6ac64fb6c656de
fuego161 <46138241+fuego161@users.noreply.github.com>
2025-06-20 08:51:07 +0100

    Add mechanics tax table output

---
+++ src/app/mechanics/page.tsx
@@ -0,0 +1,7 @@
+import { MechanicsView } from "@/modules/mechanics/ui/views/mechanics-view";
+
+const Page = async () => {
+	return <MechanicsView />;
+};
+
+export default Page;
---
+++ src/modules/mechanics/ui/views/mechanics-view.tsx
@@ -0,0 +1,5 @@
+import { TaxonomyTable } from "@/components/ui/taxonomy-table";
+
+export const MechanicsView = () => {
+	return <TaxonomyTable taxonomy="mechanic" />;
+};
