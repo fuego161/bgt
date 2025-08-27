e07b0a824a4f940a6eab4820f47c34a03e80e114
fuego161 <46138241+fuego161@users.noreply.github.com>
2025-08-27 09:24:36 +0100

    Update font used to "Hanken Grotesk"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Hanken_Grotesk } from "next/font/google";
 
 import type { Metadata } from "next";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+const hankenGrotesk = Hanken_Grotesk({
+	variable: "--font-hanken-grotesk",
+	weight: ["400", "600"],
 	subsets: ["latin"],
 });
 
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`${hankenGrotesk.variable} antialiased`}>
 				<section className="w-full px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
 					<div className="wrapper">{children}</div>
 				</section>
