c66838ae4c57e52f39fbb86981333b22916a8195
fuego161 <46138241+fuego161@users.noreply.github.com>
2025-09-02 08:43:09 +0100

    Update image output method and space section

--- src/modules/home/ui/sections/intro/index.tsx
+++ src/modules/home/ui/sections/intro/index.tsx
@@ -1,11 +1,50 @@
 import Image from "next/image";
 import { buttonVariants } from "@/components/ui/button/button-variants";
 
-const imagePaths = [""];
+interface ImageData {
+	fileName: string;
+	altText: string;
+	classList: string;
+}
+
+const imageData: ImageData[] = [
+	{
+		fileName: "azul",
+		altText: "Azul",
+		classList: "w-11/42 sm:w-8/42 md:w-5/42 lg:w-4/42",
+	},
+	{
+		fileName: "root",
+		altText: "Root",
+		classList: "w-15/42 sm:w-11/42 md:w-8/42 lg:w-6/42",
+	},
+	{
+		fileName: "arcs",
+		altText: "Arcs",
+		classList: "w-11/42 sm:w-11/42 md:w-11/42 lg:w-8/42",
+	},
+	{
+		fileName: "gloomhaven",
+		altText: "Gloomhaven",
+		classList: "hidden sm:block sm:w-8/42 md:w-8/42 lg:w-8/42",
+	},
+	{
+		fileName: "pandemic-legacy",
+		altText: "Pandemic Legacy",
+		classList: "hidden md:block md:w-5/42 lg:w-6/42",
+	},
+	{
+		fileName: "hero-realms",
+		altText: "Hero Realms",
+		classList: "hidden lg:block lg:w-4/42",
+	},
+];
 
 export const Intro = () => {
+	const marginTop = "mt-8 sm:mt-12 md:mt-14 lg:mt-18";
+
 	return (
-		<div className="overflow-hidden">
+		<div className={`overflow-hidden ${marginTop}`}>
 			<div className="text-center max-w-3xl m-auto">
 				<h1 className="font-main text-2xl md:text-3xl lg:text-5xl">
 					Track, Explore & Play More
@@ -35,54 +74,19 @@ export const Intro = () => {
 				</div>
 			</div>
 
-			<div className="flex justify-between items-baseline w-[110%] ml-[-5%]">
-				<Image
-					className="rounded-lg w-11/42 sm:w-8/42 md:w-5/42 lg:w-4/42"
-					src="/home/gloomhaven.png"
-					width={500}
-					height={500}
-					alt="Gloomhaven"
-				/>
-
-				<Image
-					className="rounded-lg w-15/42 sm:w-11/42 md:w-8/42 lg:w-6/42"
-					src="/home/gloomhaven.png"
-					width={500}
-					height={500}
-					alt="Gloomhaven"
-				/>
-
-				<Image
-					className="rounded-lg w-11/42 sm:w-11/42 md:w-11/42 lg:w-8/42"
-					src="/home/gloomhaven.png"
-					width={500}
-					height={500}
-					alt="Gloomhaven"
-				/>
-
-				<Image
-					className="rounded-lg hidden sm:block sm:w-8/42 md:w-8/42 lg:w-8/42"
-					src="/home/gloomhaven.png"
-					width={500}
-					height={500}
-					alt="Gloomhaven"
-				/>
-
-				<Image
-					className="rounded-lg hidden md:block md:w-5/42 lg:w-6/42"
-					src="/home/gloomhaven.png"
-					width={500}
-					height={500}
-					alt="Gloomhaven"
-				/>
-
-				<Image
-					className="rounded-lg hidden lg:block lg:w-4/42"
-					src="/home/gloomhaven.png"
-					width={500}
-					height={500}
-					alt="Gloomhaven"
-				/>
+			<div
+				className={`flex justify-between items-baseline w-[110%] ml-[-5%] ${marginTop}`}
+			>
+				{imageData.map((image) => (
+					<Image
+						key={image.fileName}
+						className={`rounded-lg ${image.classList}`}
+						src={`/home/${image.fileName}.png`}
+						width={300}
+						height={200}
+						alt={image.altText}
+					/>
+				))}
 			</div>
 		</div>
 	);
